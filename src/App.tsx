fdb65193e053a10390fb41c883038e4a875d3001
庄骥南 <a492073467@gmail.com>
2020-05-16 16:43:58 +0800

    引入 React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,66 @@
 import React from 'react';
-import './App.scss';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div/>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/tag">标签页</Link>
+              </li>
+              <li>
+                <Link to="/money">记账页</Link>
+              </li>
+              <li>
+                <Link to="/statistics">统计页</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/tag">
+              <Tag />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="money" />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tag() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function NoMatch(){
+  return (
+      <div>无法找的此页面，请确定网址是否正确</div>
+  )
+}
 export default App;
