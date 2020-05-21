43d7be82bcf7f174926eb7b1bd82f34e0e66b579
庄骥南 <a492073467@gmail.com>
2020-05-21 08:38:29 +0800

    Layout 组件完成、Views 分离完成

--- src/App.tsx
+++ src/App.tsx
@@ -5,64 +5,31 @@ import {
   Route,
   Redirect
 } from "react-router-dom";
-import styled from 'styled-components'
-import Nav from "./components/Nav";
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`
-
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`
-
-
+import Tags from "views/Tags";
+import Money from "views/Money";
+import Statistics from "views/Statistics";
+import NoMatch from "views/NoMatch";
 
 function App() {
   return (
       <Router>
-        <Wrapper>
-          <Main>
           <Switch>
             <Route path="/tag">
-              <Tag />
+              <Tags/>
             </Route>
             <Route path="/money">
-              <Money />
+              <Money/>
             </Route>
             <Route path="/statistics">
-              <Statistics />
+              <Statistics/>
             </Route>
             <Redirect exact from="/" to="money" />
             <Route path="*">
-              <NoMatch />
+              <NoMatch/>
             </Route>
           </Switch>
-          </Main>
-          <Nav/>
-        </Wrapper>
       </Router>
   );
 }
 
-function Statistics() {
-  return <h2>统计页面</h2>;
-}
-
-function Tag() {
-  return <h2>标签页面</h2>;
-}
-
-function Money() {
-  return <h2>记账页面</h2>;
-}
-
-function NoMatch(){
-  return (
-      <div>无法找的此页面，请确定网址是否正确</div>
-  )
-}
 export default App;
---
+++ src/views/NoMatch.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NoMatch(){
+    return (
+        <div>无法找的此页面，请确定网址是否正确</div>
+    )
+}
+
+export default NoMatch
